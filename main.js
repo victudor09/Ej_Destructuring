33a90a43cfecd8eba991f94ee3b28c4b42cdfcc4
victudor09 <victudor09@gmail.com>
2024-05-19 00:00:36 +0200

    Version2

--- main.js
+++ main.js
@@ -51,7 +51,73 @@ console.log(moves[1]);
 
 //2. EJERCICIOS SPREAD Y REST
 // PDTES.
+const pikachu = {
+    name: "Pikachu",
+    type: "electric",
+    ability: {
+    "primary": "Static",
+    "hidden": "Lightning rod"
+    },
+    stats: {
+    hp: 35,
+    attack: 55,
+    deffense: 40,
+    speed: 90
+    },
+    moves: [
+    "Quick Attack", "Volt Tackle", "Iron Tail", "Thunderbolt"
+    ],
+}
 
+const mergeObjets =  {...pokemon, ...pikachu}
+console.log(mergeObjets);
+   
+//
 
+function sumEveryOther(...numeros){
+    let suma = 0
+    numeros.forEach((numero) => {
+        suma = suma + numero
+    })      
+    console.log(suma);
+} 
 
+sumEveryOther(6, 8, 2, 3, 1);
+sumEveryOther(11, 3, 12);
 
+//
+
+function addOnlyNums(...parametros){
+    let suma = 0
+    parametros.forEach((parametro) => {
+        if (+parametro){
+            suma = suma + parametro
+        }
+    })      
+    console.log(suma);
+}
+addOnlyNums(1, 'perro', 2, 4); 
+
+//
+
+function countTheArgs(...argumentos){
+    let num_argumentos = 0
+    argumentos.forEach((argumento) => {
+        num_argumentos = num_argumentos + 1
+    })
+    console.log(num_argumentos);
+}
+countTheArgs('gato', 'perro'); //2
+countTheArgs('gato', 'perro', 'pollo', 'oso'); //4
+
+//
+
+function combineTwoArrays(array1, array2) {
+    const combineArrays = [...array1, ...array2];
+    console.log(combineArrays);
+}
+
+const arrayComida = ['patata', 'tomate', 'lechuga']
+const arrayRandom = [1, true, 'chocolate']
+
+combineTwoArrays(arrayComida, arrayRandom)
